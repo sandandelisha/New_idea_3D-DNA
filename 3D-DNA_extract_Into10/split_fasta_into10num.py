d1e4266374234bf641b727b49203a6243a4e698c
sandandelisha <120235016+sandandelisha@users.noreply.github.com>
2023-04-28 15:27:14 +0800

    Create split_fasta_into10num.py
    把reference文件（fasta文件）经过samtools处理后的fasta.fai文件，按照大小和数量平均分为10份。这十份中每份contigs数量相似，每份contigs加在一起的大小也相似。
    
    
    终端输入：python split_fasta_into10num.py yourfastafile.fai

---
+++ 3D-DNA_extract_Into10/split_fasta_into10num.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+import sys
+
+size_file = sys.argv[1]
+
+
+size = []
+with open(size_file) as f:
+    for line in f:
+        line = line.strip().split()
+        size.append([line[0], int(line[1])])
+
+
+part = {}
+length = {i: 1 for i in range(1, 11)}
+
+
+for i in range(len(size)):
+    
+    len_ = [[j, length[j]] for j in range(1, 11)]
+    len_.sort(key=lambda x: x[1])   
+    part[len_[0][0]] = part.get(len_[0][0], []) + [size[i]]
+    length[len_[0][0]] += size[i][1]
+
+
+for j in range(1, 11):
+    with open(size_file + '.' + str(j), 'w') as f:
+        for ctg in part.get(j, []):
+           f.write('{}\t{}\n'.format(ctg[0], ctg[1]))
