aee5e10c17af3231ef87461c083cdfb50f62f479
sandandelisha <120235016+sandandelisha@users.noreply.github.com>
2023-04-28 15:33:33 +0800

    Create split_merged_nodups.txt.py
    按照10个分出来的.fai文件，把merged_nodups.txt分为10份。由于merged.nodups.txt文件中可能有很多没用的信息，我们只需要两个contig都存在于….中的行，即第二列和第六列中的contig名称都存在于….中的提取出来。

---
+++ 3D-DNA_extract_Into10/split_merged_nodups.txt.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+# 创建目录
+output_dir = "output_files"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# 创建空字典列表，用于存储 ctgID 文件中第一列的值
+hash_dicts = [{} for _ in range(10)]
+
+# 分别打开 ctgID 文件，并将其中的第一列值添加到相应的字典中
+for i in range(1, 11):
+    ctg_file_name = "Vmanfinal.curated.fasta.small.fa.chrlist.{}".format(i)
+    with open(ctg_file_name, 'r') as ctg_file:
+        for line in ctg_file:
+            line = line.strip()
+            if not line:
+                continue
+            fields = line.split()
+            hash_dicts[i-1][fields[0]] = 1
+
+# 分别打开 sort 文件，并匹配其中第二列和第六列的值是否在 ctgID 中出现过
+# 如果出现过，则将该行写入相应的输出文件中
+sort_file_name = sys.argv[1]
+with open(sort_file_name, 'r') as sort:
+    out_files = [open(os.path.join(output_dir, "merged_nodups.{}.txt".format(i)), 'w') for i in range(1, 11)]
+    for line in sort:
+        line = line.strip()
+        fields = line.split()
+        for i, hash_dict in enumerate(hash_dicts):
+            if hash_dict.get(fields[1]) and hash_dict.get(fields[5]):
+                out_files[i].write(line + "\n")
+    for out_file in out_files:
+        out_file.close()
+
